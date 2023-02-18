87075b719f7383159b2833a3d30d14dbd961823c
fkeyz <fkeyzuwu@gmail.com>
2023-02-18 17:09:58 +0200

    chill

--- Card.gd
+++ Card.gd
@@ -16,6 +16,7 @@ class_name Card extends Control
 		if !card_data.card_data_changed.is_connected(update_card_data):
 			card_data.card_data_changed.connect(update_card_data)
 
+@export_category("Current Stats")
 @export var health: int:
 	set(value):
 		health = value
@@ -32,6 +33,10 @@ class_name Card extends Control
 		if not is_inside_tree():
 			await ready
 		attack_text.text = str(attack)
+
+@export_category("FX")
+@export var heal_particle_effect: PackedScene
+@export var damage_particle_effect: PackedScene
 
 func _ready() -> void:
 	if card_data == null:
@@ -120,14 +125,23 @@ func switch_container(container_name: String):
 
 func damage_card(card: Card):
 	card.health -= self.attack
+	card.spawn_particle(damage_particle_effect)
 	var damage_event = DamageEvent.new(self.attack, card, self)
 	EventManager.invoke_event(damage_event)
 	
 func heal_card(card: Card):
 	card.health += self.health
+	card.spawn_particle(heal_particle_effect)
 	var heal_event = HealEvent.new(self.health, card, self)
 	EventManager.invoke_event(heal_event)
 
 func kill_card():
 	EventManager.invoke_event(MonsterDiedEvent.new(self))
 	queue_free()
+
+func spawn_particle(particle_effect: PackedScene):
+	var particle = particle_effect.instantiate() as GPUParticles2D
+	particle.position = get_global_mouse_position()
+	particle.rotation = rotation
+	particle.emitting = true
+	get_tree().current_scene.add_child(particle)
